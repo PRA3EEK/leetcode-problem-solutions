7d65e9dd93412da13cf05e88f4fe5f46bce177ad
PRATEEK CHAUHAN <105979795+PRA3EEK@users.noreply.github.com>
2023-02-15 18:18:37 +0530

    added a one more faster solution

--- Construct_binary_tree_from_preorder_and_inorder.java
+++ Construct_binary_tree_from_preorder_and_inorder.java
@@ -42,3 +42,34 @@ class Solution {
          return root;
     }
 }
+
+
+class Solution {
+
+    Map<Integer, Integer> inorderMap = new HashMap<>();
+   
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        
+        for(int i=0; i<inorder.length; i++) inorderMap.put(inorder[i], i);
+
+        return createTree(inorder, 0, inorder.length - 1, preorder, 0, preorder.length - 1);
+        
+    }
+
+    public TreeNode createTree(int[] inorder, int is, int ie, int[] preorder, int ps, int pe){
+   
+
+        if(ps>pe || is>ie) return null;
+
+
+        TreeNode root = new TreeNode(preorder[ps]);
+        int curPos = inorderMap.get(root.val);
+        int onLeft = curPos - is;
+
+        root.left = createTree(inorder, is, curPos - 1, preorder, ps + 1, ps + onLeft);
+        root.right = createTree(inorder, curPos + 1, ie, preorder, ps + onLeft + 1, pe);
+
+        return root;
+    }
+}
